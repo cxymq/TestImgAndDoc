6dba860459312170df8d42bef63b047bee06aa0a
nazimai <809670160@qq.com>
2020-06-04 17:23:09 +0800

    Initial Commit

---
+++ Test/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Test
+//
+//  Created by nazimai on 2020/6/4.
+//  Copyright © 2020 nazimai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by nazimai on 2020/6/4.
+//  Copyright © 2020 nazimai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
